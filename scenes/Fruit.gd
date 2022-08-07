0c577a5783999754b0b92813df5675dbbd575b99
efenstakes <efenstakes101@gmail.com>
2022-08-07 18:55:26 +0300

    add Tween to animate after being hit and queue free when done

--- scenes/Fruit.gd
+++ scenes/Fruit.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 
+onready var SliceTween = $SliceTween
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	register_animation()
 
 
 func _on_Fruit_area_entered(area):
@@ -14,3 +17,24 @@ func _on_Fruit_area_entered(area):
 		# disable collision shapes
 		$CollisionShape2D.disabled = true
 		
+		SliceTween.start()
+
+
+
+func register_animation():
+	SliceTween.interpolate_property(
+			$Sprite,
+			"scale",
+			$Sprite.get_scale(),
+			Vector2(0, 0),
+			.5,
+			Tween.TRANS_QUAD,
+			Tween.EASE_OUT
+	)
+	
+	
+
+
+func _on_SliceTween_tween_all_completed():
+	print("tween done, queue_free")
+	queue_free()
