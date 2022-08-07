625d79f3a1d41a4b5d3f90c7ce7929dffa580ef9
efenstakes <efenstakes101@gmail.com>
2022-08-07 18:58:07 +0300

    ase random fruit sprite texture for each fruit

--- scenes/Fruit.gd
+++ scenes/Fruit.gd
@@ -5,8 +5,37 @@ onready var SliceTween = $SliceTween
 
 
 
+const textures = [
+	"res://assets/Fruits/fruit1.png",
+	"res://assets/Fruits/fruit2.png",
+	"res://assets/Fruits/fruit3.png",
+	"res://assets/Fruits/fruit4.png",
+	"res://assets/Fruits/fruit5.png",
+	"res://assets/Fruits/fruit6.png",
+	"res://assets/Fruits/fruit7.png",
+	"res://assets/Fruits/fruit8.png",
+	"res://assets/Fruits/fruit9.png",
+	"res://assets/Fruits/fruit10.png",
+	"res://assets/Fruits/fruit11.png",
+	"res://assets/Fruits/fruit12.png",
+	"res://assets/Fruits/fruit13.png",
+	"res://assets/Fruits/fruit14.png",
+	"res://assets/Fruits/fruit15.png",
+	"res://assets/Fruits/fruit16.png",
+	"res://assets/Fruits/fruit17.png"
+]
+
+
+
+
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():	
+	$Sprite.set_texture(
+		load(
+			textures[ rand_range(0, (textures.size() - 1 )) ]
+		)
+	)
 	register_animation()
 
 
